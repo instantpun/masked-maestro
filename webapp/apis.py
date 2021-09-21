1f24a35301cfe07ff027d4df52aad8d88e467209
Aaron C. Robinett <arobinett_consulting@gmail.com>
2021-09-21 14:00:13 -0400

    add latest working model

---
+++ webapp/apis.py
@@ -0,0 +1,77 @@
+##### List of defined API routes within Flask #####
+import os
+# Import flask and template operators
+from flask import Flask, render_template, request
+import requests
+# from webapp.cfg import current_state, global_vars
+import controller as ctl
+# Define the WSGI application object
+app = Flask(__name__)
+
+#### Custom Errors ####
+
+class NotFoundError(Exception):
+  code = 404
+  description = "Resource Not Found"
+
+@app.route('/', methods = ['POST', 'GET'])
+def data():
+  current_state = ctl.get_current_state()
+  cluster_list = []
+  for env in current_state:
+    for cluster in current_state[env]['clusters']:
+      cluster_list.append(cluster)
+
+  if request.method == 'GET':
+      return render_template('form.html', clusters = cluster_list)
+  if request.method == 'POST':
+      form_data = request.form.to_dict()
+      command = "/usr/bin/echo -n " + form_data["String"] + " | kubeseal --raw --scope cluster-wide --from-file=/dev/stdin --cert " + directory + "/" + form_data["cluster"] + ""
+      encryptedSecret = os.popen(command).read()
+      return encryptedSecret
+
+
+@app.route('/template')
+def template():
+    returnTemplate = """---
+apiVersion: bitnami.com/v1alpha1
+kind: SealedSecret
+metadata:
+  annotations:
+    sealedsecrets.bitnami.com/cluster-wide: "true"
+  creationTimestamp: null
+  name: <secret_name>
+spec:
+  encryptedData:
+    <key>: <encrypted_string>
+  template:
+    metadata:
+      annotations:
+        sealedsecrets.bitnami.com/cluster-wide: "true"
+      creationTimestamp: null
+      name: <secret_name>
+"""
+    return returnTemplate
+
+@app.route('/cert/<string:env>')
+def cert(env):
+    """
+    :param env: This parameter should correspond to one of the top-level fields in the inventory.yaml file (e.g. 'dev','test','prod', etc)
+    :type: str
+    """
+    try:
+      current_state = ctl.get_current_state()
+
+      if not current_state.get(env): 
+        raise NotFoundError('No resource was found for the env provided')
+      
+      return current_state[env]['master_cert']
+
+    except requests.exceptions.HTTPError as http_err:
+      print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except NotFoundError as err:
+      # render empty string, and respond with HTTP 404 code
+      return "Page Not Found", 404
+    #  print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    # except Exception as err:
+    #   print(f'Other error occurred: {err}')  # Python 3.6
---
+++ webapp/run.py
@@ -0,0 +1,50 @@
+import threading #TODO: implement multithreading
+from datetime import datetime
+import apis
+import cfg
+import controller as ctl
+
+old_print = print
+
+def timestamped_print(*args, **kwargs):
+  old_print(datetime.now(), *args, **kwargs)
+
+print = timestamped_print
+
+def main():
+  config = ctl.yaml_file_to_dict(ctl.INVENTORY_PATH)
+  print("Clusters in Configuration: ")
+  print(",".join(config['clusters'].keys())) # dict.keys() returns an iterable list of key names
+  #current_state = ctl.config_to_state(config)
+  ctl.config_to_state(config)
+  print("initial:\n" + str(ctl.get_current_state()))
+  ctl.set_defaults_in_state()
+  print("with defaults:\n" + str(ctl.get_current_state()))
+  # current_state = ctl.enforce_desired_state(current_state)
+  ctl.enforce_desired_state()
+  print("with desired:\n" + str(ctl.get_current_state()))
+  # possible one-liner = enforce_desired_state(init_default_state(yaml_file_to_dict(INVENTORY_PATH)))
+  # TODO: Implement control loop...
+  
+  # retry_counter = 0 # self-terminate if failed 5 times
+  # 
+  # while current_state != desired_state:
+  #
+  #     enforce_desired_state(default_state)
+  #     if current_state == desired_state:
+  #         retry_counter = 0 # reset counter
+  #
+  #         
+  #         sleep(POLL_TIME_SEC) # sleep 1 hr and check again
+  #         continue
+  #     else:
+  #         retry_counter += 1
+  #         assert retry_counter < RETRY_LIMIT, "Unable to establish desired state after 5 attempts, terminating..."
+  #         
+
+# if __name__ == '__main__':
+
+# print(cfg.current_state)
+main()
+apis.app.run(host='0.0.0.0', port=8080, debug=True)
+# print(cfg.current_state)
