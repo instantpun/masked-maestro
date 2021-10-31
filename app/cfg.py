adbe27966e7c366e42f8d2ab23fe38bbf9f713a6
Aaron C. Robinett <arobinett_consulting@gmail.com>
2021-10-31 11:52:21 -0400

    add K8S_INVENTORY_PATH to global config

--- app/cfg.py
+++ app/cfg.py
@@ -11,6 +11,7 @@ global_vars = dict()
 
 # flag DEBUG=True if '--debug' was passed as 1st cli argument
 #DEBUG = True if len(sys.argv) > 1 and sys.argv[1] == '--debug' else False
+K8S_TEMPLATE_PATH = os.environ.get('K8S_TEMPLATE_PATH') if os.environ.get('K8S_TEMPLATE_PATH') else 'k8s_templates/'
 INVENTORY_PATH = os.environ.get('INVENTORY_PATH') # shell var should be defined in k8s deployment
 WORKING_DIR = "/tmp/certs" # shell var should be defined in k8s deployment
 POLL_TIME_SEC = int(os.environ.get('POLL_TIME_SEC')) if os.environ.get('POLL_TIME_SEC') else 600 # time = seconds, default 600 sec
